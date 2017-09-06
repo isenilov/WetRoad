8d00cc395cd1e7e1580a5671879922f703f46b02
isenilov <isenilov@gmail.com>
2017-09-06 16:44:01 +0300

    Added MFCC features option

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,15 +1,26 @@
 from scipy.io import wavfile
 import numpy as np
 from scipy import signal
+from librosa import feature
 
 
-def extract(wav_file, nfft=64, window_length=0.03):
+def extract(wav_file, nfft=64, window_length=0.03, mel=False):
     rate, frames = wavfile.read(wav_file)
     window = round(window_length * rate)
     feat = []
 
     for i in range(0, len(frames)-window, window):
-        freqs, bins, pxx = signal.spectrogram(frames[i:i + window - 1], rate, nperseg=nfft, noverlap=round(nfft/2))
+        if mel:
+            pxx = feature.mfcc(frames[i:i + window - 1],
+                               sr=rate,
+                               n_fft=nfft,
+                               hop_length=round(nfft / 2))
+        else:
+            _, _, pxx = signal.spectrogram(frames[i:i + window - 1],
+                                           rate,
+                                           nperseg=nfft,
+                                           noverlap=round(nfft / 2))
+
         feat.append(pxx.flatten())
 
     return np.stack(feat)
