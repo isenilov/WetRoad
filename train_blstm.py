2cdecb8bc76155add77681101d531fef91844043
isenilov <isenilov@gmail.com>
2017-09-08 20:42:21 +0300

    LSTM architecture

--- train_blstm.py
+++ train_blstm.py
@@ -1,82 +1,63 @@
 from feature_extraction import extract_features
-import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import recall_score, accuracy_score
-import sklearn.preprocessing
-import time
+from time import time
 from keras.models import Sequential
-import keras.layers
+from keras.layers import LSTM, Dense
+from keras.layers.wrappers import Bidirectional
 from keras.callbacks import TensorBoard
 from keras import optimizers
+from keras.utils import to_categorical
 
 
-start = time.time()
+start = time()
 print("\nExtracting features...")
-features, labels = extract_features(flatten=False)
-print(features.shape, labels.shape)
-end = time.time()
+X_train, y_train = extract_features("dataset/wet1/audio_mono.wav", "dataset/dry1/audio_mono.wav", flatten=False)
+X_test, y_test = extract_features("dataset/wet2/audio_mono.wav", "dataset/dry2/audio_mono.wav", flatten=False)
+X_val, y_val = extract_features("dataset/wet3/audio_mono.wav", "dataset/dry3/audio_mono.wav", flatten=False)
+end = time()
 print("Took %.3f sec." % (end - start))
 
-start = time.time()
-print("\nNormalizing features...")
-#features = sklearn.preprocessing.scale(features)
-end = time.time()
-print("Took %.3f sec." % (end - start))
+# start = time()
+# print("\nSplitting dataset...")
+# X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+# print(y_train, y_test)
+# end = time()
+# print("Took %.3f sec." % (end - start))
 
-start = time.time()
-print("\nSplitting dataset...")
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-end = time.time()
-print("Took %.3f sec." % (end - start))
-
-start = time.time()
+start = time()
 print("\nTraining model...")
 tbCallback = TensorBoard()
 
+# architecture of the network is adopted from https://arxiv.org/pdf/1511.07035.pdf
 model = Sequential()
 
-model.add(keras.layers.Conv1D(256, input_shape=(X_train.shape[1], X_train.shape[2]), kernel_size=5, activation='tanh'))
-model.add(keras.layers.Conv1D(256, kernel_size=3, activation='tanh'))
-model.add(keras.layers.MaxPooling1D(pool_size=3))
-model.add(keras.layers.LSTM(256, return_sequences=True, activation='tanh'))
-model.add(keras.layers.LSTM(256, return_sequences=True, activation='tanh'))
-model.add(keras.layers.LSTM(256, activation='tanh'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
+model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh"),
+                        input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh")))
+model.add(Bidirectional(LSTM(216, activation="tanh")))
+model.add(Dense(2, activation='softmax'))
 
-# model.add(keras.layers.LSTM(16, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True, activation="sigmoid"))
-# model.add(keras.layers.Dropout(0.2))
-# model.add(keras.layers.LSTM(32, return_sequences=False, activation="sigmoid"))
-# model.add(keras.layers.Dropout(0.2))
-# model.add(keras.layers.Dense(1, activation="sigmoid"))
-#
-# model.add(keras.layers.Dense(256, input_dim=X_train.shape[1], activation='relu'))
-# model.add(keras.layers.Dropout(0.2))
-# model.add(keras.layers.Dense(256, activation='relu'))
-# model.add(keras.layers.Dropout(0.2))
-# model.add(keras.layers.Dense(256, activation='relu'))
-# model.add(keras.layers.Dropout(0.2))
-# model.add(keras.layers.Dense(1, activation="sigmoid"))
+optimizer = optimizers.Adam(lr=1e-5)
 
-optimizer = optimizers.SGD(lr=1e-5)
-
-model.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
 
 model.fit(X_train, y_train,
+          validation_data=(X_val, y_val),
           callbacks=[tbCallback],
-          epochs=200,
-          batch_size=512,
+          epochs=50,
+          batch_size=128,
           verbose=1)
 
-end = time.time()
+end = time()
 print("\nTook %.3f sec." % (end - start))
 
-start = time.time()
+start = time()
 print("\nEvaluating...")
-y_pred = model.predict_classes(X_test, verbose=1)
+y_pred = to_categorical(model.predict_classes(X_test, verbose=1))
 print(y_pred, y_test)
-print("\nAccuracy: %s" % accuracy_score(y_test, y_pred))
-print("Recall (wet): %s" % recall_score(y_test, y_pred))
-end = time.time()
+print("\nAccuracy:", accuracy_score(y_test, y_pred))
+print("Recall (wet):", recall_score(y_test, y_pred, average="macro"))
+end = time()
 print("Took %.3f sec." % (end - start))
