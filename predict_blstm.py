09dd425c79ef3b598d0f6963978aafa748690b9c
isenilov <isenilov@gmail.com>
2017-09-12 12:06:55 +0300

    Predicting using BLSTM with latest weights

---
+++ predict_blstm.py
@@ -0,0 +1,54 @@
+from feature_extraction import extract_features, get_last_weights
+from sklearn.metrics import recall_score, accuracy_score
+from time import time
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.layers.wrappers import Bidirectional
+from keras.callbacks import TensorBoard
+from keras import optimizers
+from keras.utils import to_categorical
+
+
+start = time()
+print("\nExtracting features...")
+
+# X_test, y_test = extract_features("dataset/wet3/audio_mono.wav",
+#                                   "dataset/dry3/audio_mono.wav", flatten=False, scaling=False)
+X_test, y_test = extract_features("dataset/wet/test_wet.wav",
+                                  "dataset/dry/test_dry.wav", flatten=False, scaling=False)
+
+end = time()
+print("Took %.3f sec." % (end - start))
+
+start = time()
+print("\nTraining model...")
+tbCallback = TensorBoard()
+
+# architecture of the network is adopted from https://arxiv.org/pdf/1511.07035.pdf
+model = Sequential()
+
+model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh"),
+                        input_shape=(X_test.shape[1], X_test.shape[2])))
+model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh")))
+model.add(Bidirectional(LSTM(216, activation="tanh")))
+model.add(Dense(2, activation='softmax'))
+
+optimizer = optimizers.Adam(lr=1e-5)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              metrics=['accuracy'])
+weights = get_last_weights("weights/")
+model.load_weights(weights)
+print("Using wights:", weights)
+end = time()
+print("\nTook %.3f sec." % (end - start))
+
+start = time()
+print("\nEvaluating...")
+y_pred = to_categorical(model.predict_classes(X_test, verbose=1))
+print(y_pred, y_test)
+print("\nAccuracy:", accuracy_score(y_test, y_pred))
+print("Recall (wet):", recall_score(y_test, y_pred, average="macro"))
+end = time()
+print("Took %.3f sec." % (end - start))
