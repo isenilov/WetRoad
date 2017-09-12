56dd93b2bc948218f39858db47bfc584e3f041f4
isenilov <isenilov@gmail.com>
2017-09-12 12:23:10 +0300

    Loads last weights before training, saves after

--- train_blstm.py
+++ train_blstm.py
@@ -1,4 +1,4 @@
-from feature_extraction import extract_features
+from feature_extraction import extract_features, get_last_weights
 from sklearn.metrics import recall_score, accuracy_score
 from time import time
 from keras.models import Sequential
@@ -7,16 +7,25 @@ from keras.layers.wrappers import Bidirectional
 from keras.callbacks import TensorBoard
 from keras import optimizers
 from keras.utils import to_categorical
+from datetime import datetime
 
 
 start = time()
 print("\nExtracting features...")
+# X_train, y_train = extract_features("dataset/wet/test_wet.wav",
+#                                     "dataset/dry/test_dry.wav", flatten=False, scaling=False)
+# X_test, y_test = extract_features("dataset/wet/test_wet.wav",
+#                                   "dataset/dry/test_dry.wav", flatten=False, scaling=False)
+# X_val, y_val = extract_features("dataset/wet/test_wet.wav",
+#                                 "dataset/dry/test_dry.wav", flatten=False, scaling=False)
+
+
 X_train, y_train = extract_features("dataset/wet1/audio_mono.wav",
-                                    "dataset/dry1/audio_mono.wav", flatten=False, scaling=True)
+                                    "dataset/dry1/audio_mono.wav", flatten=False, scaling=False)
 X_test, y_test = extract_features("dataset/wet2/audio_mono.wav",
-                                  "dataset/dry2/audio_mono.wav", flatten=False, scaling=True)
+                                  "dataset/dry2/audio_mono.wav", flatten=False, scaling=False)
 X_val, y_val = extract_features("dataset/wet3/audio_mono.wav",
-                                "dataset/dry3/audio_mono.wav", flatten=False, scaling=True)
+                                "dataset/dry3/audio_mono.wav", flatten=False, scaling=False)
 end = time()
 print("Took %.3f sec." % (end - start))
 
@@ -33,26 +42,25 @@ tbCallback = TensorBoard()
 
 # architecture of the network is adopted from https://arxiv.org/pdf/1511.07035.pdf
 model = Sequential()
-
 model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh"),
                         input_shape=(X_train.shape[1], X_train.shape[2])))
 model.add(Bidirectional(LSTM(216, return_sequences=True, activation="tanh")))
 model.add(Bidirectional(LSTM(216, activation="tanh")))
 model.add(Dense(2, activation='softmax'))
-
 optimizer = optimizers.Adam(lr=1e-5)
-
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
-
+weights = get_last_weights("weights/")
+model.load_weights(weights)
+print("Using wights:", weights)
 model.fit(X_train, y_train,
           validation_data=(X_val, y_val),
           callbacks=[tbCallback],
           epochs=50,
           batch_size=128,
           verbose=1)
-
+model.save_weights("weights/weights " + datetime.now().strftime("%d-%m-%Y %H:%M") + ".h5")
 end = time()
 print("\nTook %.3f sec." % (end - start))
 
