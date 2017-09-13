4bce210d0e0417543d4b17ee1a8ca3497a48479e
Ivan <isenilov@gmail.com>
2017-09-13 19:46:45 +0300

    Added comments

--- gcloud_env_setup.sh
+++ gcloud_env_setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # setup of environmet on Google Cloud VM instance
 
+# set up of Google FUSE (storage bucket mounting tool for Google Cloud)
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -8,14 +9,17 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update -y --force-yes -qq
 sudo apt-get install gcsfuse -y --force-yes -qq
 
+# installation of Miniconda
 cd ~
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
+# mounting of dataset bucket
 cd WetRoad
 mkdir models
 mkdir dataset
 gcsfuse --implicit-dirs wetroad_dataset ~/WetRoad/dataset
 
+# putting to .bashrc to mount every time it starts
 echo '# mounting of dataset bucket' >> ~/.bashrc
 echo 'gcsfuse --implicit-dirs wetroad_dataset ~/WetRoad/dataset' >> ~/.bashrc
