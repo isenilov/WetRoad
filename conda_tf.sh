23ab94e59d18fa9845a019aa95bc7450bcb8a699
Ivan <isenilov@gmail.com>
2017-09-13 19:44:04 +0300

    Update conda_tf.sh
    Added comments

--- conda_tf.sh
+++ conda_tf.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# installing Anaconda packages
 conda install -y scikit-learn keras
 conda install -y -c conda-forge librosa
 
+# preparing env for bazel
 sudo apt-get install openjdk-8-jdk -y --force-yes -qq
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get install oracle-java8-installer -y --force-yes -qq
@@ -12,6 +14,7 @@ curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 
 sudo apt-get update && sudo apt-get install bazel -y --force-yes -qq
 
+# building tensorflow
 cd ~
 git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
