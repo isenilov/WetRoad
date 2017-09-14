9d6bffc5e8f9f0b8c14416fb20f65c7ebeece137
isenilov <isenilov@gmail.com>
2017-09-14 19:12:26 +0300

    Fixed configuration

--- conda_tf.sh
+++ conda_tf.sh
@@ -19,5 +19,5 @@ cd ~
 git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
 ./configure
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 -k //tensorflow/tools/pip_package:build_pip_package
+bazel build --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 -k //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
