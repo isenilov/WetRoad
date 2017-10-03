b2dbcbdd63b3f1c8560c30f6e53f587f61768695
isenilov <isenilov@gmail.com>
2017-10-03 21:47:00 +0300

    Added writing model summary to file, continuing of experiments

--- train_blstm.py
+++ train_blstm.py
@@ -14,13 +14,20 @@ from datetime import datetime
 
 start = time()
 print("\nExtracting features...")
-X_1, y_1 = extract_features("dataset/wet/chevy_wet.wav",
-                            "dataset/dry/chevy_dry.wav", flatten=False, scaling=False)
-X_2, y_2 = extract_features("dataset/wet1/audio_mono.wav",
+X_1, y_1 = extract_features("dataset/wet1/audio_mono.wav",
                             "dataset/dry1/audio_mono.wav", flatten=False, scaling=False)
-X = np.concatenate((X_1, X_2))
-y = np.concatenate((y_1, y_2))
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_2, y_2 = extract_features("dataset/wet2/audio_mono.wav",
+                            "dataset/dry2/audio_mono.wav", flatten=False, scaling=False)
+X_3, y_3 = extract_features("dataset/wet3/audio_mono.wav",
+                            "dataset/dry3/audio_mono.wav", flatten=False, scaling=False)
+
+X_train = np.concatenate((X_1, X_2, X_3))
+y_train = np.concatenate((y_1, y_2, y_3))
+
+X_test, y_test = extract_features("dataset/wet/chevy_wet.wav",
+                                  "dataset/dry/chevy_dry.wav", flatten=False, scaling=False)
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 # X_train, y_train = extract_features("dataset/wet/test_wet.wav",
 #                                     "dataset/dry/test_dry.wav", flatten=False, scaling=False)
@@ -63,27 +70,38 @@ model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
 weights = get_last("models/", "weights")
-model.load_weights(weights)
+if weights is not None:
+    model.load_weights(weights)
 print("Using weights:", weights)
 model.fit(X_train, y_train,
 #          validation_data=(X_val, y_val),
           callbacks=[tbCallback],
-          epochs=25,
+          epochs=1,
           batch_size=128,
-          verbose=1)
+          verbose=2)
 
-dt = datetime.now().strftime("%d-%m-%Y %H:%M")
-model.save_weights("models/weights " + dt + ".h5")
-with open("models/model " + dt + ".yaml", "w") as model_yaml:
+dt = datetime.now().strftime("%d-%m-%Y %H-%M")
+weights_filename = "models/weights " + dt + ".h5"
+model.save_weights(weights_filename)
+model_filename = "models/model " + dt + ".yaml"
+with open(model_filename, "w") as model_yaml:
     model_yaml.write(model.to_yaml())
 end = time()
-print("\nTook %.3f sec." % (end - start))
+training_time = end - start
+print("\nTook %.3f sec." % training_time)
 
 start = time()
 print("\nEvaluating...")
 y_pred = to_categorical(model.predict_classes(X_test, verbose=1))
 print(y_pred, y_test)
-print("\nAccuracy:", accuracy_score(y_test, y_pred))
-print("Recall (wet):", recall_score(y_test, y_pred, average="macro"))
+acc = accuracy_score(y_test, y_pred)
+print("\nAccuracy:", acc)
+rec = recall_score(y_test, y_pred, average="macro")
+print("Recall (wet):", rec)
 end = time()
 print("Took %.3f sec." % (end - start))
+
+with open('results.txt', 'a') as f:
+    f.write("BLSTM " + dt + " Input shape: " + str(X_train.shape) + " Accuracy: " + str(acc) +
+            " Reacall: " + str(rec) + " Training time: " + str(training_time) + " s\n")
+    model.summary(print_fn=lambda x: f.write(x + '\n'))
