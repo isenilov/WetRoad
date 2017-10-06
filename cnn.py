f74a1517760923ccc053031d3ff904ae9ece7acd
isenilov <isenilov@gmail.com>
2017-10-06 21:02:56 +0300

    Tuning

--- cnn.py
+++ cnn.py
@@ -71,14 +71,14 @@ try:
     model.add(Conv1D(filters=1, kernel_size=3, strides=2,
                      input_shape=X_train.shape[1:], kernel_initializer='uniform',
                      activation='tanh', kernel_regularizer='l2'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(Conv1D(64, 3, activation='tanh', kernel_regularizer='l2'))
     model.add(MaxPooling1D(3))
     model.add(Conv1D(128, 3, activation='tanh', kernel_regularizer='l2'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(Conv1D(256, 3, activation='tanh', kernel_regularizer='l2'))
     model.add(GlobalAveragePooling1D())
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(Dense(2, activation='sigmoid'))
 
     model.compile(loss='categorical_crossentropy',
