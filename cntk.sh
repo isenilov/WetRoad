5a003ecfbc00114b8b6c0411b6fa8bb224b50ed1
isenilov <isenilov@gmail.com>
2017-10-16 15:08:25 +0300

    CNTK build script (not finished)

---
+++ cntk.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# sudo apt-get install openmpi-bin
+# pip install https://cntk.ai/PythonWheel/CPU-Only/cntk-2.2-cp36-cp36m-linux_x86_64.whl
+# /home/isenilov/miniconda3/lib/python3.6/site-packages/cntk
+
+sudo mkdir /usr/local/CNTKCustomMKL
