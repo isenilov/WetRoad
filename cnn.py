dd6086d924b5887d551f2c4e9a08a06ce9341f81
isenilov <isenilov@gmail.com>
2017-11-04 10:21:14 +0200

    added pitch shift, added convolutional dropout and 1 BLSTM

--- cnn.py
+++ cnn.py
@@ -17,6 +17,8 @@ import os
 def def_model_cnn_blstm(input_shape):
     model = Sequential()
     model.add(TimeDistributed(Conv1D(filters=8, kernel_size=64, strides=2, activation='relu'), input_shape=input_shape))
+    model.add(TimeDistributed(MaxPooling1D(4)))
+    model.add(TimeDistributed(Dropout(0.3)))
     model.add(TimeDistributed(Conv1D(32, 32, activation='relu')))
     model.add(TimeDistributed(MaxPooling1D(4)))
     model.add(TimeDistributed(Dropout(0.3)))
