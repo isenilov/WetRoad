385450203e6b34d928841faaf769995ae56be46b
isenilov <isenilov@gmail.com>
2017-11-05 08:56:31 +0200

    printing model and weights filenames

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import numpy as np
 from feature_extraction import extract_features, get_last
 
 model = get_last("", "model")
-if model is not None:
+weights = get_last("", "weights")
+if model is not None and weights is not None:
     yaml_file = open(model, "r")
     loaded_model_file = yaml_file.read()
     yaml_file.close()
     loaded_model = model_from_yaml(loaded_model_file)
-    loaded_model.load_weights(get_last("", "weights"))
-    loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+    loaded_model.load_weights(weights)
+    print("Using model: " + model, "\nUsing weights: " + weights + "\n")
+    loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     X_test, y_test = extract_features("dataset/wet2/audio_mono.wav", "dataset/dry2/audio_mono.wav",
                                       mel=False, flatten=False, scaling=True, categorical=True)
     X_test = np.expand_dims(X_test, axis=1)
@@ -18,6 +19,6 @@ if model is not None:
     X_test = np.expand_dims(X_test, axis=3)
 
     score = loaded_model.evaluate(X_test, y_test, verbose=1)
-    print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
+    print("\n%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
 
 
