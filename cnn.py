f7549ccb88cf39d935fdaa28e7f2f76548f486f4
isenilov <isenilov@gmail.com>
2017-11-08 07:36:23 +0200

    swap of train/test sets

--- cnn.py
+++ cnn.py
@@ -168,9 +168,9 @@ def ex_feat():
     #                                   mel=False, flatten=False, scaling=True, categorical=True)
     # X_val, y_val = extract_features("dataset/wet/test_wet.wav", "dataset/dry/test_dry.wav",
     #                                 mel=False, flatten=False, scaling=True, categorical=True)
-    X_train, y_train = shuffle(extract_features("dataset/wet/chevy_wet.wav", "dataset/dry/chevy_dry.wav",
+    X_train, y_train = shuffle(extract_features("dataset/wet3/audio_mono.wav", "dataset/dry3/audio_mono.wav",
                                         mel=False, flatten=False, scaling=True, categorical=True), random_state=1)
-    X_test, y_test = extract_features("dataset/wet3/audio_mono.wav", "dataset/dry3/audio_mono.wav",
+    X_test, y_test = extract_features("dataset/wet/chevy_wet.wav", "dataset/dry/chevy_dry.wav",
                                       mel=False, flatten=False, scaling=True, categorical=True)
     X_val, y_val = extract_features("dataset/wet2/audio_mono.wav", "dataset/dry2/audio_mono.wav",
                                     mel=False, flatten=False, scaling=True, categorical=True)
