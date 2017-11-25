a7bcfe90bdbf223f2fe8a4317d1789cba3b67d0e
isenilov <isenilov@gmail.com>
2017-11-25 20:07:41 +0200

    experiment with big file

--- bigcnn.py
+++ bigcnn.py
@@ -19,7 +19,7 @@ import soundfile as sf
 dt = datetime.now().strftime("%d-%m-%Y.%H-%M")
 N = 4096  # length of feature vector
 B = 256  # batch size
-S = 1000000000  # steps per epoch
+S = 100000000  # steps per epoch
 
 
 class TestCallback(Callback):
