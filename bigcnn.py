d6c1843660e05dae8fe96b1850370e076b9ffcdd
isenilov <isenilov@gmail.com>
2017-11-25 21:42:32 +0200

    experiment with big file

--- bigcnn.py
+++ bigcnn.py
@@ -44,7 +44,6 @@ def generator(w, d, batch_size=128):
             data.append(d.__next__())
             labels.append(0)
         data = np.array(data)
-        print(data.shape)
         data = data[:,:,0]
         data = np.expand_dims(data, axis=1)
         data = data.reshape((data.shape[0], 1, data.shape[2]))
