8a20abb22cbd0ac4050e755db463ed10861d87fe
isenilov <isenilov@gmail.com>
2017-11-26 13:14:21 +0200

    experiment with big file

--- bigcnn.py
+++ bigcnn.py
@@ -24,9 +24,9 @@ S = 100  # steps per epoch
 
 def generator(w, d, batch_size=128):
     i = 0
-    w = sf.read(w, frames=N, start=i)
+    w, _ = sf.read(w, frames=N, start=i)
     print(w)
-    d = sf.read(d, frames=N, start=i)
+    d, _ = sf.read(d, frames=N, start=i)
     while 1:
         data = []
         labels = []
