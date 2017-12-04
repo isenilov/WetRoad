48857f2ebe1e55b27b891d9a501f07a0275218d3
isenilov <isenilov@gmail.com>
2017-12-04 14:45:59 +0200

    visualize model from yaml

--- vis.py
+++ vis.py
@@ -1,5 +1,14 @@
 from cnn import def_model_cnn_blstm
+from feature_extraction import get_last
 from keras.utils import plot_model
+from keras.models import model_from_yaml
 
-model = def_model_cnn_blstm((1, 16384, 1))
-plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=False)
+model = get_last("", "model")
+if model is not None:
+    yaml_file = open(model, "r")
+    loaded_model_file = yaml_file.read()
+    yaml_file.close()
+    loaded_model = model_from_yaml(loaded_model_file)
+    plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=False)
+# model = def_model_cnn_blstm((1, 16384, 1))
+
